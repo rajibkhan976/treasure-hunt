763bceaa17c0e38084cab4ab6f130c7386e09aa0
unknown <rajibkhan976@gmail.com>
2020-04-19 20:53:47 +0200

    API call

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-*
+/*
 * Function that initiates the whole Game application.
 */
 function init(){
@@ -10,6 +10,8 @@ function initGameUI(){
 }
 
 function initChests(){
+	let img = document.createElement('img');
+	chestsDiv.appendChild(img);
 }
 
 function initScoreBoard(){
@@ -29,6 +31,18 @@ function chestClicked(e){
 
 function getImageFromPexels(){
   // make a request towards pexels API and get 1 Diamond image
+  // 563492ad6f917000010000014304512fa34a457ba983f0558cfe579a
+  var xhr = new XMLHttpRequest();
+  var page = Math.floor(Math.random() * (1000 - 1 + 1)) + 1;
+  xhr.onreadystatechange  = function () {
+	  if (this.readyState == 4 && this.status == 200) {
+		  var result = JSON.parse(this.responseText);
+		  console.log(result);
+		}
+	};
+  xhr.open('GET', `https://api.pexels.com/v1/curated?per_page=1&page=${page}`, true);
+  xhr.setRequestHeader('Authorization', '563492ad6f917000010000014304512fa34a457ba983f0558cfe579a');
+  xhr.send();
 }
 
 function refresh(){
@@ -36,3 +50,8 @@ function refresh(){
 
 function removeChestEvents(){
 }
+
+var chestsDiv = document.getElementById("chests");
+var refreshButton = document.getElementById("refresh-button");
+chestsDiv.addEventListener("click", initChests);
+refreshButton.addEventListener("click", getImageFromPexels);
