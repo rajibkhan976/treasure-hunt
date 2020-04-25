852d76bd6d0b4417d5ce6a5b5d2e1baa3833dc53
unknown <rajibkhan976@gmail.com>
2020-04-25 20:12:21 +0200

    complete solution with comments 4

--- u2_js_rah_kha.js
+++ u2_js_rah_kha.js
@@ -1,11 +1,14 @@
 /*
 * Function that initiates the whole Game application.
 */
-
-var chestsDiv = document.getElementById("chests");
-let score = 0;
+var chestsDiv = document.getElementById("chests"); //capturing the div element by id
+var refreshButton = document.getElementById("refresh-button"); //capturing the refresh button by id
+let score = 0; //initializing the score 
+//adding the game initiator event while window loads for
+window.addEventListener("load", init);
 
 function init(){
+  //initiating the game user interface
   initGameUI();
 }
 
@@ -17,58 +20,62 @@ function initGameUI(e){
 }
 
 function initChests(e){
-	let imageOne = document.createElement('img');
+	//initiating the chests setup
+	let imageOne = document.createElement('img'); //creating an image tag
 	imageOne.id = 'image-one';
 	imageOne.src = 'images/chest-closed.png';
 	imageOne.style = 'width: 15em; height: 10em';
-	let imageTwo = document.createElement('img');
+	let imageTwo = document.createElement('img'); //creating an image tag
 	imageTwo.id = 'image-two';
 	imageTwo.src = 'images/chest-closed.png';
 	imageTwo.style = 'width: 15em; height: 10em; margin-left: 1em;';
-	let imageThree = document.createElement('img');
+	let imageThree = document.createElement('img'); //creating an image tag
 	imageThree.id = 'image-three';
 	imageThree.src = 'images/chest-closed.png';
 	imageThree.style = 'width: 15em; height: 10em; margin-left: 1em;';
 	chestsDiv.appendChild(imageOne);
 	chestsDiv.appendChild(imageTwo);
 	chestsDiv.appendChild(imageThree);
-	var imageOneTag = document.getElementById("image-one");
-	var imageTwoTag = document.getElementById("image-two");
-	var imageThreeTag = document.getElementById("image-three");
+	var imageOneTag = document.getElementById("image-one"); //capturing the image element by its id
+	var imageTwoTag = document.getElementById("image-two"); //capturing the image element by its id
+	var imageThreeTag = document.getElementById("image-three"); //capturing the image element by its id
 	initChestEventListeners(imageOneTag, imageTwoTag, imageThreeTag, e);
 }
 
 function initScoreBoard(){
-	let scoreCounter = document.createElement('p');
+	// initializing and setting up the score board
+	let scoreCounter = document.createElement('p'); //creating a p element to show the score
 	scoreCounter.id = 'score-counter';
 	scoreCounter.innerHTML = `Score: ${score}`;
 	scoreCounter.style = 'display: block; color: white; margin: 1em auto; font-size: 1.2em;';
-	var refreshButton = document.getElementById("refresh-button");
 	refreshButton.after(scoreCounter);
 }
 
 function initRefreshButton(){
-	var refreshButton = document.getElementById("refresh-button");
+	//adding the click event to the refresh button
 	refreshButton.addEventListener("click", refresh);
 }
 
 function initChestEventListeners(imageOneTag, imageTwoTag, imageThreeTag, e) {
+	//adding click events to the chests images
 	imageOneTag.addEventListener("click", function () {chestClicked(1, e)}, false);
 	imageTwoTag.addEventListener("click", function () {chestClicked(2, e)}, false);
 	imageThreeTag.addEventListener("click", function () {chestClicked(3, e)}, false);
 }
 
 function placeTreassure(imageOneTag, imageTwoTag, imageThreeTag, imageIdentifier, randomNumber){
-	
+	//palcing the treasure image based on the result of random matches
 	getImageFromPexels();
 	
-	let scoreCounter = document.getElementById("score-counter");
-	let data = sessionStorage.getItem('result');
-	let imageTreasure = JSON.parse(data);
-	let treasureImage = document.createElement('img');
+	let scoreCounter = document.getElementById("score-counter"); //capturing the p tag of scoreboard by id
+	let data = sessionStorage.getItem('result'); //storing the data fetched from the remote api
+	let imageTreasure = JSON.parse(data); //storing the parsed data in JSON format
+	let treasureImage = document.createElement('img'); //creating the treasure image tag
 	
 	if (imageTreasure !== undefined &&
+	imageTreasure !== null &&
 	imageTreasure.photos !== undefined &&
+	imageTreasure.photos !== null &&
 	imageTreasure.photos.length !== 0) {
 		imageTreasure.photos.map((value, index) => {
 			treasureImage.src = `${value.src.original}`;
@@ -100,14 +107,14 @@ function placeTreassure(imageOneTag, imageTwoTag, imageThreeTag, imageIdentifier
 }
 
 function chestClicked(imageIdentifier, e){
+	//checking the random matches between the players guess and the position of the treasure
+	let randomNumber = Math.floor(Math.random() * (3 - 1 + 1)) + 1; //storing a random number between 1-3
 	
-	let randomNumber = Math.floor(Math.random() * (3 - 1 + 1)) + 1;
+	var imageOneTag = document.getElementById("image-one"); //capturing the image tag by id
+	var imageTwoTag = document.getElementById("image-two"); //capturing the image tag by id
+	var imageThreeTag = document.getElementById("image-three"); //capturing the image tag by id
 	
-	var imageOneTag = document.getElementById("image-one");
-	var imageTwoTag = document.getElementById("image-two");
-	var imageThreeTag = document.getElementById("image-three");
-	
-	let replaceImage = document.createElement('img');
+	let replaceImage = document.createElement('img'); //creating the opened chest image tag
 	replaceImage.src = 'images/chest-open.png';
 	
 	if (imageIdentifier !== randomNumber) {
@@ -134,13 +141,12 @@ function chestClicked(imageIdentifier, e){
 
 function getImageFromPexels(){
   // make a request towards pexels API and get 1 Diamond image
-  // 563492ad6f917000010000014304512fa34a457ba983f0558cfe579a
-  var xhr = new XMLHttpRequest();
-  var page = Math.floor(Math.random() * (1000 - 1 + 1)) + 1;
+  var xhr = new XMLHttpRequest(); //initializing a new request
+  var page = Math.floor(Math.random() * (1000 - 1 + 1)) + 1; //string the random page number
   xhr.onreadystatechange  = function () {
 	  if (this.readyState == 4 && this.status == 200) {
-		  var result = this.responseText;
-		  sessionStorage.setItem('result', result);
+		  var result = this.responseText; //storing the response data
+		  sessionStorage.setItem('result', result); //storing the result into session
 		}
 	};
   xhr.open('GET', `https://api.pexels.com/v1/curated?per_page=1&page=${page}`, true);
@@ -149,10 +155,10 @@ function getImageFromPexels(){
 }
 
 function refresh(e){
-	
-	var imageOneTag = document.getElementById("image-one");
-	var imageTwoTag = document.getElementById("image-two");
-	var imageThreeTag = document.getElementById("image-three");
+	//refresing the chests images
+	var imageOneTag = document.getElementById("image-one"); //capturing the image tag by id
+	var imageTwoTag = document.getElementById("image-two"); //capturing the image tag by id
+	var imageThreeTag = document.getElementById("image-three"); //capturing the image tag by id
 	chestsDiv.removeChild(imageOneTag);
 	chestsDiv.removeChild(imageTwoTag);
 	chestsDiv.removeChild(imageThreeTag);
@@ -160,9 +166,8 @@ function refresh(e){
 }
 
 function removeChestEvents(imageOneTag, imageTwoTag, imageThreeTag){
+	//removing the click events from the chests iimages
 	imageOneTag.removeEventListener("click", function () {chestClicked(1, e)}, false);
 	imageTwoTag.removeEventListener("click", function () {chestClicked(2, e)}, false);
 	imageThreeTag.removeEventListener("click", function () {chestClicked(3, e)}, false);
 }
-
-window.addEventListener("load", init);
