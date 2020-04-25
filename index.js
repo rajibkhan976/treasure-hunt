e984c8013291ba7a26134aff9d1f24a2903c116e
unknown <rajibkhan976@gmail.com>
2020-04-25 17:33:45 +0200

    partial solution 2

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 /*
 * Function that initiates the whole Game application.
 */
+
+var chestsDiv = document.getElementById("chests");
+let score = 0;
+
 function init(){
   initGameUI();
 }
 
-function initGameUI(){
+function initGameUI(e){
   // Call functions that creates the Game UI
+  initChests(e);
+  initRefreshButton();
+  initScoreBoard();
 }
 
 function initChests(e){
@@ -28,60 +35,100 @@ function initChests(e){
 	var imageOneTag = document.getElementById("image-one");
 	var imageTwoTag = document.getElementById("image-two");
 	var imageThreeTag = document.getElementById("image-three");
+	initChestEventListeners(imageOneTag, imageTwoTag, imageThreeTag, e);
+}
+
+function initScoreBoard(){
+	let scoreCounter = document.createElement('p');
+	scoreCounter.id = 'score-counter';
+	scoreCounter.innerHTML = `Score: ${score}`;
+	scoreCounter.style = 'display: block; color: white; margin: 1em auto; font-size: 1.2em;';
+	var refreshButton = document.getElementById("refresh-button");
+	refreshButton.after(scoreCounter);
+}
+
+function initRefreshButton(){
+	var refreshButton = document.getElementById("refresh-button");
+	refreshButton.addEventListener("click", refresh);
+}
+
+function initChestEventListeners(imageOneTag, imageTwoTag, imageThreeTag, e) {
 	imageOneTag.addEventListener("click", function () {chestClicked(1, e)}, false);
 	imageTwoTag.addEventListener("click", function () {chestClicked(2, e)}, false);
 	imageThreeTag.addEventListener("click", function () {chestClicked(3, e)}, false);
 }
 
-function initScoreBoard(){
-}
-
-function initRefreshButton(){
-}
-
-function initChestEventListeners() {
-}
-
-function placeTreassure(){
+function placeTreassure(imageOneTag, imageTwoTag, imageThreeTag, imageIdentifier, randomNumber){
+	
+	getImageFromPexels();
+	
+	let scoreCounter = document.getElementById("score-counter");
+	let data = sessionStorage.getItem('result');
+	let imageTreasure = JSON.parse(data);
+	let treasureImage = document.createElement('img');
+	
+	if (imageTreasure !== undefined &&
+	imageTreasure.photos !== undefined &&
+	imageTreasure.photos.length !== 0) {
+		imageTreasure.photos.map((value, index) => {
+			treasureImage.src = `${value.src.original}`;
+		});
+	}
+	
+	if (imageIdentifier === 1 && randomNumber === 1) {
+		treasureImage.id = 'image-one';
+		treasureImage.style = 'width: 15em; height: 10em;';
+		chestsDiv.replaceChild(treasureImage, imageOneTag);
+		score += 5;
+		scoreCounter.innerHTML = `Score: ${score}`;
+		removeChestEvents(imageOneTag, imageTwoTag, imageThreeTag);
+	} else if (imageIdentifier === 2 && randomNumber === 2) {
+		treasureImage.id = 'image-two';
+		treasureImage.style = 'width: 15em; height: 10em; margin-left: 1em;';
+		chestsDiv.replaceChild(treasureImage, imageTwoTag);
+		score += 5;
+		scoreCounter.innerHTML = `Score: ${score}`;
+		removeChestEvents(imageOneTag, imageTwoTag, imageThreeTag);
+	} else {
+		treasureImage.id = 'image-three';
+		treasureImage.style = 'width: 15em; height: 10em; margin-left: 1em;';
+		chestsDiv.replaceChild(treasureImage, imageThreeTag);
+		score += 5;
+		scoreCounter.innerHTML = `Score: ${score}`;
+		removeChestEvents(imageOneTag, imageTwoTag, imageThreeTag);
+	}
 }
 
 function chestClicked(imageIdentifier, e){
+	
 	let randomNumber = Math.floor(Math.random() * (3 - 1 + 1)) + 1;
+	
 	var imageOneTag = document.getElementById("image-one");
 	var imageTwoTag = document.getElementById("image-two");
 	var imageThreeTag = document.getElementById("image-three");
+	
 	let replaceImage = document.createElement('img');
-	replaceImage.id = 'image-replace';
 	replaceImage.src = 'images/chest-open.png';
 	
-	let treasureImage = document.createElement('img');
-	treasureImage.id = 'image-treasure';
-	treasureImage.src = 'images/chest-jewel.png';
-	
-	console.log(imageIdentifier);
-	console.log(randomNumber);
 	if (imageIdentifier !== randomNumber) {
 		if (imageIdentifier === 1) {
-			treasureImage.style = 'width: 15em; height: 10em;';
-			chestsDiv.replaceChild(treasureImage, imageOneTag);
-		} else if (imageIdentifier === 2) {
-			treasureImage.style = 'width: 15em; height: 10em; margin-left: 1em;';
-			chestsDiv.replaceChild(treasureImage, imageTwoTag);
-		} else {
-			treasureImage.style = 'width: 15em; height: 10em; margin-left: 1em;';
-			chestsDiv.replaceChild(treasureImage, imageThreeTag);
-		}
-	} else {
-		if (imageIdentifier === 1) {
+			replaceImage.id = 'image-one';
 			replaceImage.style = 'width: 15em; height: 10em;';
 			chestsDiv.replaceChild(replaceImage, imageOneTag);
+			removeChestEvents(imageOneTag, imageTwoTag, imageThreeTag);
 		} else if (imageIdentifier === 2) {
+			replaceImage.id = 'image-two';
 			replaceImage.style = 'width: 15em; height: 10em; margin-left: 1em;';
 			chestsDiv.replaceChild(replaceImage, imageTwoTag);
+			removeChestEvents(imageOneTag, imageTwoTag, imageThreeTag);
 		} else {
+			replaceImage.id = 'image-three';
 			replaceImage.style = 'width: 15em; height: 10em; margin-left: 1em;';
 			chestsDiv.replaceChild(replaceImage, imageThreeTag);
+			removeChestEvents(imageOneTag, imageTwoTag, imageThreeTag);
 		}
+	} else {
+		placeTreassure(imageOneTag, imageTwoTag, imageThreeTag, imageIdentifier, randomNumber);
 	}
 }
 
@@ -92,8 +139,8 @@ function getImageFromPexels(){
   var page = Math.floor(Math.random() * (1000 - 1 + 1)) + 1;
   xhr.onreadystatechange  = function () {
 	  if (this.readyState == 4 && this.status == 200) {
-		  var result = JSON.parse(this.responseText);
-		  console.log(result);
+		  var result = this.responseText;
+		  sessionStorage.setItem('result', result);
 		}
 	};
   xhr.open('GET', `https://api.pexels.com/v1/curated?per_page=1&page=${page}`, true);
@@ -101,14 +148,25 @@ function getImageFromPexels(){
   xhr.send();
 }
 
-function refresh(){
+function refresh(e){
+	if (chestsDiv.hasChildNodes()) {
+		let chestsDivChilds = chestsDiv.childNodes;
+		for (let i = 0; i < chestsDivChilds.length; i++) {
+			if (chestsDivChilds[i].src !== 'images/chest-closed.png') {
+				chestsDivChilds[i].src = 'images/chest-closed.png';
+			}
+		}
+		var imageOneTag = document.getElementById("image-one");
+		var imageTwoTag = document.getElementById("image-two");
+		var imageThreeTag = document.getElementById("image-three");
+		initChestEventListeners(imageOneTag, imageTwoTag, imageThreeTag, e);
+	}
 }
 
-function removeChestEvents(){
+function removeChestEvents(imageOneTag, imageTwoTag, imageThreeTag){
+	imageOneTag.removeEventListener("click", function () {chestClicked(1, e)}, false);
+	imageTwoTag.removeEventListener("click", function () {chestClicked(2, e)}, false);
+	imageThreeTag.removeEventListener("click", function () {chestClicked(3, e)}, false);
 }
 
-var chestsDiv = document.getElementById("chests");
-var refreshButton = document.getElementById("refresh-button");
-
-window.addEventListener("load", initChests);
-refreshButton.addEventListener("click", getImageFromPexels);
+window.addEventListener("load", init);
