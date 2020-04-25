da9d6afe5c22f6f4b10d3463215692e1184c0760
unknown <rajibkhan976@gmail.com>
2020-04-25 11:05:47 +0200

    partial solution 1

--- index.js
+++ index.js
@@ -9,9 +9,28 @@ function initGameUI(){
   // Call functions that creates the Game UI
 }
 
-function initChests(){
-	let img = document.createElement('img');
-	chestsDiv.appendChild(img);
+function initChests(e){
+	let imageOne = document.createElement('img');
+	imageOne.id = 'image-one';
+	imageOne.src = 'images/chest-closed.png';
+	imageOne.style = 'width: 15em; height: 10em';
+	let imageTwo = document.createElement('img');
+	imageTwo.id = 'image-two';
+	imageTwo.src = 'images/chest-closed.png';
+	imageTwo.style = 'width: 15em; height: 10em; margin-left: 1em;';
+	let imageThree = document.createElement('img');
+	imageThree.id = 'image-three';
+	imageThree.src = 'images/chest-closed.png';
+	imageThree.style = 'width: 15em; height: 10em; margin-left: 1em;';
+	chestsDiv.appendChild(imageOne);
+	chestsDiv.appendChild(imageTwo);
+	chestsDiv.appendChild(imageThree);
+	var imageOneTag = document.getElementById("image-one");
+	var imageTwoTag = document.getElementById("image-two");
+	var imageThreeTag = document.getElementById("image-three");
+	imageOneTag.addEventListener("click", function () {chestClicked(1, e)}, false);
+	imageTwoTag.addEventListener("click", function () {chestClicked(2, e)}, false);
+	imageThreeTag.addEventListener("click", function () {chestClicked(3, e)}, false);
 }
 
 function initScoreBoard(){
@@ -26,7 +45,44 @@ function initChestEventListeners() {
 function placeTreassure(){
 }
 
-function chestClicked(e){
+function chestClicked(imageIdentifier, e){
+	let randomNumber = Math.floor(Math.random() * (3 - 1 + 1)) + 1;
+	var imageOneTag = document.getElementById("image-one");
+	var imageTwoTag = document.getElementById("image-two");
+	var imageThreeTag = document.getElementById("image-three");
+	let replaceImage = document.createElement('img');
+	replaceImage.id = 'image-replace';
+	replaceImage.src = 'images/chest-open.png';
+	
+	let treasureImage = document.createElement('img');
+	treasureImage.id = 'image-treasure';
+	treasureImage.src = 'images/chest-jewel.png';
+	
+	console.log(imageIdentifier);
+	console.log(randomNumber);
+	if (imageIdentifier !== randomNumber) {
+		if (imageIdentifier === 1) {
+			treasureImage.style = 'width: 15em; height: 10em;';
+			chestsDiv.replaceChild(treasureImage, imageOneTag);
+		} else if (imageIdentifier === 2) {
+			treasureImage.style = 'width: 15em; height: 10em; margin-left: 1em;';
+			chestsDiv.replaceChild(treasureImage, imageTwoTag);
+		} else {
+			treasureImage.style = 'width: 15em; height: 10em; margin-left: 1em;';
+			chestsDiv.replaceChild(treasureImage, imageThreeTag);
+		}
+	} else {
+		if (imageIdentifier === 1) {
+			replaceImage.style = 'width: 15em; height: 10em;';
+			chestsDiv.replaceChild(replaceImage, imageOneTag);
+		} else if (imageIdentifier === 2) {
+			replaceImage.style = 'width: 15em; height: 10em; margin-left: 1em;';
+			chestsDiv.replaceChild(replaceImage, imageTwoTag);
+		} else {
+			replaceImage.style = 'width: 15em; height: 10em; margin-left: 1em;';
+			chestsDiv.replaceChild(replaceImage, imageThreeTag);
+		}
+	}
 }
 
 function getImageFromPexels(){
@@ -53,5 +109,6 @@ function removeChestEvents(){
 
 var chestsDiv = document.getElementById("chests");
 var refreshButton = document.getElementById("refresh-button");
-chestsDiv.addEventListener("click", initChests);
+
+window.addEventListener("load", initChests);
 refreshButton.addEventListener("click", getImageFromPexels);
